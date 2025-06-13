2dd407a79f261526651d40f4a0909a93baa8b99c
Pushpa-2002 <khaniyasujita1@gmail.com>
2025-06-13 17:04:04 +0545

    added fireflies background in success

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -6,7 +6,16 @@ export default function Success(){
       const { ref, isInView } = useInView();
     return (
         <>
-          <div ref={ref} id='success' className="bg-gradient-to-br from-gray-300 to-green-300 text-white w-full h-[100vh] flex flex-col space-y-10 p-10 justify-center items-center">
+          <div ref={ref} id='success' className="relative bg-gradient-to-br from-gray-300 to-green-300 text-white w-full h-[100vh] flex flex-col space-y-10 p-10 justify-center items-center">
+
+           {/* Fireflies background */}
+  <div className="absolute inset-0 z-0 pointer-events-none">
+    {[...Array(16)].map((_, i) => (
+      <span key={i} className="firefly" />
+    ))}
+  </div>
+
+  {/* Foreground content */}
           <h2 className={`text-8xl font-bold  ${isInView ? 'animate-from-right' : 'opacity-0'} `}>SUCCESS</h2>
           <p className={`text-2xl font-medium ${isInView ? 'animate-from-top':'opacity-0'}`}>Living your dream, building your future in the Gulf</p>
         <div className={`grid grid-cols-3 gap-7 ${isInView ? 'animate-scale' : 'opacity-0' } `}>
